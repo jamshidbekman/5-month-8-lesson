6ea10816d3ca2fac9832adb4f18d163d8f5b0631
Jamshidbek Mansurov <97334536+jamshidbekman@users.noreply.github.com>
2024-12-26 17:14:40 +0500

    Update author.service.js

--- src/service/author.service.js
+++ src/service/author.service.js
@@ -1,5 +1,6 @@
 import authorModel from "../models/author.model.js";
 import bookModel from "../models/book.model.js";
+import mongoose from "mongoose";
 
 class AuthorService {
   constructor() {
@@ -12,7 +13,75 @@ class AuthorService {
   }
   async getAuthors({ name, id }) {
     if (name) {
+      const authors = this.authorModel.aggregate([
+        {
+          $match: { name: name },
+        },
+        {
+          $lookup: {
+            localField: "_id",
+            foreignField: "authorId",
+            from: "books",
+            as: "books",
+            pipeline: [
+              {
+                $project: {
+                  _id: "$_id",
+                  title: "$title",
+                  pages: "$pages",
+                  year: "$year",
+                },
+              },
+            ],
+          },
+        },
+        {
+          $project: {
+            _id: "$_id",
+            authorName: "$name",
+            totalBooks: { $size: "$books" },
+            totalPages: { $sum: "$books.pages" },
+            avgPages: { $avg: "$books.pages" },
+            books: "$books",
+          },
+        },
+      ]);
+      return authors;
     } else if (id) {
+      const authors = this.authorModel.aggregate([
+        {
+          $match: { _id: new mongoose.Types.ObjectId(id) },
+        },
+        {
+          $lookup: {
+            localField: "_id",
+            foreignField: "authorId",
+            from: "books",
+            as: "books",
+            pipeline: [
+              {
+                $project: {
+                  _id: "$_id",
+                  title: "$title",
+                  pages: "$pages",
+                  year: "$year",
+                },
+              },
+            ],
+          },
+        },
+        {
+          $project: {
+            _id: "$_id",
+            authorName: "$name",
+            totalBooks: { $size: "$books" },
+            totalPages: { $sum: "$books.pages" },
+            avgPages: { $avg: "$books.pages" },
+            books: "$books",
+          },
+        },
+      ]);
+      return authors;
     } else {
       const authors = this.authorModel.aggregate([
         {
